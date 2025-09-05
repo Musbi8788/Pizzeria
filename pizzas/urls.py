ff7e45c681052b2c3c4859575dcb092f3eeef70e
Musbi <musbi700@gmail.com>
2025-09-05 02:05:02 +0100

    updated

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-my_app = 'pizzas'
+app_name = 'pizzas'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('pizzas/', views.pizzas, name='pizzas'),
+    path('pizzas/', views.my_pizzas, name='my_pizzas'),
+    path('pizzas/<int:pizza_id>', views.pizza, name='pizza'),
 
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -7,8 +7,15 @@ from .models import Pizza
 def index(request):
     return render(request, "pizzas/index.html")
 
-def pizzas(request):
-    """Represent to displaying Pizzas avaible in the database"""
-    pizzas = Pizza.objects.all()
+def my_pizzas(request):
+    """Show all pizzas"""
+    pizzas = Pizza.objects.order_by('date_added')
     contexts = {'pizzas': pizzas}
-    return render(request, "pizzas/pizzas.html", contexts)
+    return render(request, "pizzas/my_pizzas.html", contexts)
+
+def pizza(request, pizza_id):
+    """Show a signle pizza with it entries."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name') # topping_set is used because of the have topping model name
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, "pizzas/pizza.html", context)
